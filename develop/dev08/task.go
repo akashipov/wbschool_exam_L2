7ad68ecf2aa602c93631aa1f4dc6dd1ac8df86b9
Artem Kashipov <drinkwaterknow@gmail.com>
2024-03-05 00:12:50 +0400

    added large task dev08 with tests

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/chigopher/pathlib"
+	proc "github.com/mitchellh/go-ps"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -14,6 +31,506 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Shell struct {
+	PreviousPath *pathlib.Path
+	CurrentDir   *pathlib.Path
+	stdin        io.Reader
+	stdout       io.Writer
+	stderr       io.Writer
+}
 
+func (s *Shell) cd(args []string) {
+	// Я мог тут работать с PWD env переменной, но не стал переделывать старый вариант
+	// наверное тут хотели чтобы я работал с подгрузкой переменных через os и изменением их
+	if !s.CurrentDir.IsAbsolute() {
+		r := make(chan string, 1)
+		s.pwd(r)
+		s.CurrentDir = pathlib.NewPath(<-r)
+	}
+	if len(args) == 0 {
+		fmt.Println("Wrong count of arguments for 'cd' command")
+	}
+	home := pathlib.NewPath(os.Getenv("HOME"))
+	if args[0] == "" {
+		s.PreviousPath = s.CurrentDir
+		s.CurrentDir = home
+	} else if args[0] == "-" {
+		if s.PreviousPath != nil {
+			s.CurrentDir, s.PreviousPath = s.PreviousPath, s.CurrentDir
+		}
+	} else if args[0] == ".." {
+		s.PreviousPath = pathlib.NewPath(s.CurrentDir.String())
+		s.CurrentDir = s.CurrentDir.Parent()
+	} else if args[0] == "." {
+		s.PreviousPath = s.CurrentDir
+	} else if newP := pathlib.NewPath(args[0]); newP.IsAbsolute() {
+		s.PreviousPath = s.CurrentDir
+		s.CurrentDir = newP
+	} else if args[0] == "~/" {
+		s.PreviousPath = s.CurrentDir
+		s.CurrentDir = home
+	} else {
+		newP := pathlib.NewPath(filepath.Join(s.CurrentDir.String(), args[0]))
+		isDir, err := newP.IsDir()
+		if !isDir || err != nil {
+			s.errorPrint("it is not directory")
+			return
+		}
+		dirExists, err := newP.DirExists()
+		if !dirExists || err != nil {
+			s.errorPrint("Directory doesn't exist")
+			return
+		}
+		s.PreviousPath = s.CurrentDir
+		s.CurrentDir = newP
+	}
+}
+
+func (s *Shell) pwd(p chan string) {
+	f, err := filepath.Abs(s.CurrentDir.String())
+	if err != nil {
+		s.errorPrint("it is problem extract absolute path for current directory")
+		return
+	}
+	p <- f
+}
+
+func (s *Shell) echo(args []string, p chan string) {
+	p <- args[0]
+}
+
+func (s *Shell) kill(args []string) {
+	if len(args) == 0 {
+		fmt.Fprint(os.Stdout, "need to pass id number to kill\n")
+		return
+	}
+	n, err := strconv.Atoi(args[0])
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	p, err := os.FindProcess(n)
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	err = p.Kill()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	fmt.Fprint(os.Stdout, "process is successfuly killed\n")
+}
+
+func (s *Shell) ps(args []string, p chan string) {
+	// Я на mac os может тут хотели чтобы я читал файл /proc для линукс, но у меня как будто нет аналога пока
+	// или мне было сложновато найти аналог (sysctl может, но он какой то непонятный)
+	processList, err := proc.Processes()
+	if err != nil {
+		p <- "ps.Processes() Failed, are you using windows?\n"
+		return
+	}
+	b := strings.Builder{}
+	for x := range processList {
+		process := processList[x]
+		b.WriteString(fmt.Sprintf("%d\t%s\n", process.Pid(), process.Executable()))
+	}
+	p <- b.String()
+}
+
+func (s *Shell) getAbsPathToFile(p string) string {
+	path := pathlib.NewPath(p)
+	p = path.String()
+	if !path.IsAbsolute() {
+		r := make(chan string, 1)
+		s.pwd(r)
+		p = filepath.Join(<-r, p)
+	}
+	return p
+}
+
+func (s *Shell) CreateTCP(host string, port int) {
+	addr := net.TCPAddr{
+		IP:   net.ParseIP(host),
+		Port: port,
+	}
+	srv, err := net.ListenTCP("tcp", &addr)
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	for {
+		conn, err := srv.AcceptTCP()
+		if err != nil {
+			s.errorPrint(err.Error())
+			return
+		}
+		defer conn.Close()
+		for {
+			p := make([]byte, 2048)
+			_, err = conn.Read(p)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				s.errorPrint(err.Error())
+				return
+			}
+			fmt.Fprintf(s.stdout, "Have got msg: '%s'\n", string(p))
+		}
+	}
+}
+
+func (s *Shell) CreateUDP(host string, port int) {
+	addr := net.UDPAddr{
+		IP:   net.ParseIP(host),
+		Port: port,
+	}
+	srv, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	for {
+		p := make([]byte, 2048)
+		_, d, err := srv.ReadFromUDP(p)
+		if err != nil {
+			s.errorPrint(err.Error())
+			return
+		}
+		fmt.Fprintf(s.stdout, "Have got msg from '%s': '%s'\n", d, string(p))
+	}
+}
+
+func (s *Shell) dialTCP(host string, port int) {
+	raddr := &net.TCPAddr{
+		IP:   net.ParseIP(host),
+		Port: port,
+	}
+	conn, err := net.DialTCP("tcp", nil, raddr)
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	line, err := s.readFromStdin()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	_, err = conn.Write([]byte(line))
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+}
+
+func (s *Shell) dialUDP(host string, port int) {
+	raddr := &net.UDPAddr{
+		IP:   net.ParseIP(host),
+		Port: port,
+	}
+	conn, err := net.DialUDP("udp", nil, raddr)
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	line, err := s.readFromStdin()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	_, err = conn.Write([]byte(line))
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+}
+
+func (s *Shell) netcat(args []string) {
+	var toCreate, isUdp bool
+	var host string = "127.0.0.1"
+	var port int
+	for idx, a := range args {
+		switch a {
+		case "-l":
+			toCreate = true
+		case "-u":
+			isUdp = true
+		case "-h":
+			if toCreate {
+				s.errorPrint("it is not allowed to pass host to the listening server")
+				return
+			}
+			// need to check by pattern the value of it
+			if idx+1 >= len(args) {
+				s.errorPrint("need to pass host value")
+				return
+			}
+			host = args[idx+1]
+		case "-p":
+			var err error
+			if idx+1 >= len(args) {
+				s.errorPrint("need to pass port value")
+				return
+			}
+			port, err = strconv.Atoi(args[idx+1])
+			if err != nil {
+				s.errorPrint("problem with parsing of port value, please check it")
+				return
+			}
+		}
+	}
+	if toCreate {
+		if !isUdp {
+			s.CreateTCP(host, port)
+		} else {
+			s.CreateUDP(host, port)
+		}
+	} else {
+		if !isUdp {
+			s.dialTCP(host, port)
+		} else {
+			s.dialUDP(host, port)
+		}
+	}
+
+}
+
+func (s *Shell) errorPrint(msg string) string {
+	msg = strings.ToLower(msg)
+	msg = strings.TrimFunc(msg, func(r rune) bool {
+		if r == '\n' || r == ' ' {
+			return true
+		}
+		return false
+	})
+	fmt.Fprint(s.stderr, msg+"\n")
+	return msg
+}
+
+func (s *Shell) exec(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Wrong number of arguments for 'exec' command")
+		return
+	}
+	const (
+		inputSign     = "<"
+		outputSign    = ">"
+		terminalInOut = "/dev/tty"
+	)
+	if strings.HasPrefix(args[0], inputSign) {
+		p := s.getAbsPathToFile(args[0][len(inputSign):])
+		if p == terminalInOut {
+			s.stdin = os.Stdin
+			return
+		}
+		f, err := os.OpenFile(p, os.O_RDONLY|os.O_CREATE, 0777)
+		if err != nil {
+			s.errorPrint(fmt.Sprintf("problem with opening file for reading: '%s'", err.Error()))
+			return
+		}
+		s.stdin = f
+	} else if strings.HasPrefix(args[0], outputSign) {
+		p := s.getAbsPathToFile(args[0][len(outputSign):])
+		if p == terminalInOut {
+			s.stdout = os.Stdout
+			return
+		}
+		f, err := os.OpenFile(p, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
+		if err != nil {
+			s.errorPrint(
+				fmt.Sprintf("problem with opening file for writing: '%s'", err.Error()),
+			)
+			return
+		}
+		s.stdout = f
+	} else {
+		s.errorPrint(fmt.Sprintf("wrong argument for command 'exec': '%s'", args[0]))
+	}
+}
+
+func (s *Shell) readFromStdin() (string, error) {
+	reader := bufio.NewReader(s.stdin)
+	var line string
+	var err error
+	if s.stdin == os.Stdin {
+		line, err = reader.ReadString('\n')
+	} else {
+		var b []byte
+		b, err = io.ReadAll(reader)
+		if err != nil {
+			return "", err
+		}
+		_, err = s.stdin.(*os.File).Seek(0, io.SeekStart)
+		line = string(b)
+	}
+	if err != nil {
+		return "", err
+	}
+	return line, nil
+}
+
+func (s *Shell) cat(args []string, p chan string) {
+	for _, v := range args {
+		if v == "" {
+			continue
+		} else {
+			p <- v + "\n"
+			return
+		}
+	}
+	line, err := s.readFromStdin()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	p <- line
+}
+
+func (s *Shell) ReadCMDLine(f *os.File) ([]string, error) {
+	reader := bufio.NewReader(f)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	line = strings.TrimSuffix(line, "\n")
+	line = strings.TrimSpace(line)
+	pipes := strings.Split(line, " | ")
+	return pipes, nil
+}
+
+func (s *Shell) cmd(f *os.File, w *sync.WaitGroup) {
+	globalForForkW := sync.WaitGroup{}
+loop:
+	for {
+		fmt.Print(s.CurrentDir.String() + "$")
+		pipes, err := s.ReadCMDLine(f)
+		if err == io.EOF {
+			globalForForkW.Wait()
+			break
+		}
+		if err != nil {
+			log.Fatalf("something wrong with reading of input: '%s'", err.Error())
+		}
+		LastResult := make(chan string, 1)
+		for idx, pipe := range pipes {
+			arguments := strings.Split(pipe, " ")
+			if len(arguments) == 1 && arguments[0] == "" {
+				continue
+			}
+			args := []string{strings.Join(arguments[1:], " ")}
+			if idx != 0 {
+				args = append(args, <-LastResult)
+			}
+			if arguments[len(arguments)-1] == "&" {
+				s.Fork(arguments[:len(arguments)-1], LastResult, &globalForForkW)
+			} else {
+				switch arguments[0] {
+				case "cd":
+					s.cd(args)
+					LastResult <- ""
+				case "pwd":
+					s.pwd(LastResult)
+				case "echo":
+					s.echo(args, LastResult)
+				case "kill":
+					s.kill(args)
+					LastResult <- ""
+				case "ps":
+					s.ps(args, LastResult)
+				case "cat":
+					s.cat(args, LastResult)
+				case "\\q":
+					fmt.Fprint(s.stdout, "quiting from shell\n")
+					globalForForkW.Wait()
+					break loop
+				case "nc":
+					s.netcat(arguments[1:])
+					LastResult <- ""
+				case "exec":
+					s.exec(args)
+					LastResult <- ""
+				}
+			}
+			if idx == len(pipes)-1 {
+				fmt.Fprint(s.stdout, <-LastResult)
+			}
+		}
+	}
+	w.Done()
+}
+
+func (s *Shell) Fork(arguments []string, p chan string, globalW *sync.WaitGroup) {
+	// executing the process as a child process
+	if len(arguments) == 0 {
+		s.errorPrint("wrong number of arguments for fork function\n")
+		return
+	}
+	cmd := exec.Command(arguments[0], arguments[1:]...)
+	cmd.Stdin = s.stdin
+
+	// creating a std pipeline
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		s.errorPrint(err.Error())
+		return
+	}
+	fmt.Fprintf(s.stdout, "Child process with ID has been started: '%d'\n", cmd.Process.Pid)
+	globalW.Add(1)
+	go func(globalW *sync.WaitGroup) {
+		out := make([]byte, 1024)
+		w := sync.WaitGroup{}
+		w.Add(1)
+		go func() {
+			b := strings.Builder{}
+			for {
+				n, err := stdout.Read(out)
+
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					s.errorPrint(err.Error())
+					break
+				}
+				_, err = b.Write(out[:n])
+				if err != nil {
+					s.errorPrint(err.Error())
+					break
+				}
+			}
+			p <- b.String()
+			w.Done()
+		}()
+
+		w.Wait()
+		fmt.Fprintf(s.stdout, "Process with id '%d' is finished\n", cmd.Process.Pid)
+		cmd.Wait()
+		globalW.Done()
+	}(globalW)
+}
+
+func NewShell(startPath string) *Shell {
+	p := pathlib.NewPath(startPath)
+	return &Shell{
+		CurrentDir:   p,
+		PreviousPath: p,
+		stdin:        os.Stdin,
+		stdout:       os.Stdout,
+		stderr:       os.Stderr,
+	}
+}
+
+func main() {
+	shell := NewShell("./")
+	w := sync.WaitGroup{}
+	w.Add(1)
+	go shell.cmd(os.Stdin, &w)
+	w.Wait()
 }
--- develop/dev08/task_test.go
+++ develop/dev08/task_test.go
@@ -1,1 +1,434 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/chigopher/pathlib"
+)
+
+func TestShell_errorPrint(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "base01",
+			args: args{
+				msg: "hello \n",
+			},
+			want: "hello",
+		},
+		{
+			name: "base02",
+			args: args{
+				msg: "\nhello ",
+			},
+			want: "hello",
+		},
+		{
+			name: "base03",
+			args: args{
+				msg: "Hello",
+			},
+			want: "hello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := pathlib.NewPath("./")
+			s := &Shell{
+				PreviousPath: p,
+				CurrentDir:   p,
+				stdin:        os.Stdin,
+				stdout:       os.Stdout,
+				stderr:       os.Stderr,
+			}
+			r := s.errorPrint(tt.args.msg)
+			if r != tt.want {
+				t.Errorf("Wanted '%s', but has got '%s'\n", tt.want, r)
+			}
+		})
+	}
+}
+
+func TestShell_kill(t *testing.T) {
+	type fields struct {
+		PreviousPath *pathlib.Path
+		CurrentDir   *pathlib.Path
+		stdin        io.Reader
+		stdout       io.Writer
+		stderr       io.Writer
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "base01",
+			fields: fields{
+				PreviousPath: nil,
+				CurrentDir:   nil,
+				stdin:        os.Stdin,
+				stdout:       os.Stdout,
+				stderr:       os.Stderr,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Shell{
+				PreviousPath: tt.fields.PreviousPath,
+				CurrentDir:   tt.fields.CurrentDir,
+				stdin:        tt.fields.stdin,
+				stdout:       tt.fields.stdout,
+				stderr:       tt.fields.stderr,
+			}
+			cmd := exec.Command("sleep", "10")
+			cmd.Start()
+			defer func() {
+				fmt.Println("Killing by defer function at the end")
+				cmd.Process.Kill()
+			}()
+			pid := cmd.Process.Pid
+			fmt.Println(pid)
+			err := cmd.Process.Signal(syscall.Signal(0))
+			if err != nil {
+				t.Errorf("Something is wrong: %s\n", err.Error())
+			}
+			args := []string{fmt.Sprintf("%d", pid)}
+			s.kill(args)
+			p, err := os.FindProcess(pid)
+			if err != nil {
+				t.Error("Something is wrong:", err.Error())
+			}
+			state, err := p.Wait()
+			if err != nil {
+				t.Errorf("Problem with waiting")
+			}
+			fmt.Println(state)
+		})
+	}
+}
+
+func TestShell_exec(t *testing.T) {
+	type args struct {
+		filename string
+		prefix   string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "output01",
+			args: args{filename: "output.txt", prefix: ">"},
+		},
+		{
+			name: "output02",
+			args: args{filename: "/dev/tty", prefix: ">"},
+		},
+		{
+			name: "input01",
+			args: args{filename: "input.txt", prefix: "<"},
+		},
+		{
+			name: "input02",
+			args: args{filename: "/dev/tty", prefix: "<"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewShell("./")
+			args := []string{tt.args.prefix + tt.args.filename}
+			s.exec(args)
+			defer func() {
+				if strings.HasPrefix(tt.args.filename, "output") {
+					fmt.Println("Removing files after creating")
+					os.Remove(tt.args.filename)
+				}
+			}()
+			switch tt.args.prefix {
+			case ">":
+				if tt.args.filename == "/dev/tty" {
+					if s.stdout != os.Stdout {
+						t.Error("Stdout wasn't changed to default")
+					}
+					return
+				}
+				wStr := "Hello World"
+				fmt.Fprint(s.stdout, wStr)
+				f, err := os.OpenFile(tt.args.filename, os.O_RDONLY, 0)
+				if err != nil {
+					t.Error(err.Error())
+					return
+				}
+				b, err := io.ReadAll(f)
+				if err != nil {
+					t.Error(err.Error())
+					return
+				}
+				if string(b) != wStr {
+					t.Error("Stdout hasn't been changed\n")
+					return
+				}
+			case "<":
+				if tt.args.filename == "/dev/tty" {
+					if s.stdin != os.Stdin {
+						t.Error("Stdin wasn't changed to default")
+					}
+					return
+				}
+				line, err := s.readFromStdin()
+				if err != nil {
+					t.Error(err.Error())
+					return
+				}
+				exp := "blabla\nbla"
+				if line != exp {
+					t.Errorf("We have got: '%s', but should be '%s'", line, exp)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestShell_Fork(t *testing.T) {
+	type args struct {
+		arguments []string
+	}
+	output := make(chan string, 1)
+	tests := []struct {
+		name     string
+		args     args
+		want     string
+		duration time.Duration
+	}{
+		{
+			name:     "basetest01",
+			args:     args{arguments: []string{"echo", "a"}},
+			want:     "a\n",
+			duration: 0,
+		},
+		{
+			name:     "sleep02",
+			args:     args{arguments: []string{"sleep", "1"}},
+			want:     "",
+			duration: time.Second,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewShell("./")
+			s.Fork(tt.args.arguments, output, &sync.WaitGroup{})
+			Time := time.Now()
+			r := <-output
+			since := time.Since(Time)
+			if r != tt.want {
+				t.Errorf("Wanted '%s', got '%s'", tt.want, r)
+			}
+			if since <= tt.duration {
+				t.Errorf("Wanted duration '%v', got '%v'", tt.duration, since)
+			}
+		})
+	}
+}
+
+func TestShell_cd(t *testing.T) {
+	type args struct {
+		args []string
+	}
+	currentDir := pathlib.NewPath(os.Getenv("PWD"))
+	homeDir := pathlib.NewPath(os.Getenv("HOME"))
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "root01",
+			args: args{[]string{""}},
+			want: homeDir.String(),
+		},
+		{
+			name: "back02",
+			args: args{[]string{".."}},
+			want: currentDir.Parent().String(),
+		},
+		{
+			name: "back021",
+			args: args{[]string{"./.."}},
+			want: currentDir.Parent().String(),
+		},
+		{
+			name: "back022",
+			args: args{[]string{"../.."}},
+			want: currentDir.Parent().Parent().String(),
+		},
+		{
+			name: "home030",
+			args: args{[]string{"/"}},
+			want: "/",
+		},
+		{
+			name: "home031",
+			args: args{[]string{"/home"}},
+			want: "/home",
+		},
+		{
+			name: "previous04",
+			args: args{[]string{"-"}},
+			want: currentDir.String(),
+		},
+		{
+			name: "next05_file",
+			args: args{[]string{"./input.txt"}},
+			want: currentDir.String(),
+		},
+		{
+			name: "next06_dir",
+			args: args{[]string{"./test"}},
+			want: pathlib.NewPath(filepath.Join(os.Getenv("PWD"), "test")).String(),
+		},
+		{
+			name: "next07_not_exist",
+			args: args{[]string{"./abc"}},
+			want: currentDir.String(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewShell("./")
+			s.cd(tt.args.args)
+			r := make(chan string, 1)
+			s.pwd(r)
+			result := <-r
+			if tt.want != result {
+				t.Errorf("Wanted '%s', got '%s'", tt.want, result)
+			}
+		})
+	}
+}
+
+func TestShell_cmd(t *testing.T) {
+	type args struct {
+		commands string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "base01",
+			args: args{"echo a\n"},
+			want: []string{"./$a./$"},
+		},
+		{
+			name: "quit02",
+			args: args{"echo a\n\\q\n"},
+			want: []string{"./$a./$"},
+		},
+		{
+			name: "quit03",
+			args: args{"\\q\n"},
+			want: []string{"./$quiting from shell\n"},
+		},
+		{
+			name: "pipe04",
+			args: args{"echo a | echo b\n"},
+			want: []string{"./$b./$"},
+		},
+		{
+			name: "fork05",
+			args: args{"echo a &\n"},
+			want: []string{"Child process with ID has been started", "is finished"},
+		},
+		{
+			name: "sleep06",
+			args: args{"sleep 1 &\n"},
+			want: []string{"Child process with ID has been started", "is finished"},
+		},
+		{
+			name: "cd07",
+			args: args{"cd ~/\n"},
+			want: []string{fmt.Sprintf("Users/%s", os.Getenv("USER"))},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tmpfileOUT, err := os.CreateTemp("", "inputCMDout.txt")
+			if err != nil {
+				return
+			}
+			oldstdout := os.Stdout
+			os.Stdout = tmpfileOUT
+			defer func() {
+				tmpfileOUT.Close()
+				os.Remove(tmpfileOUT.Name())
+				os.Stdout = oldstdout
+			}()
+
+			tmpfileIN, err := os.CreateTemp("", "inputCMDin.txt")
+			if err != nil {
+				return
+			}
+			defer func() {
+				tmpfileIN.Close()
+				os.Remove(tmpfileIN.Name())
+			}()
+			if _, err = tmpfileIN.Write([]byte(tt.args.commands)); err != nil {
+				log.Fatal(err)
+			}
+			if _, err := tmpfileIN.Seek(0, 0); err != nil {
+				log.Fatal(err)
+			}
+			oldstdin := os.Stdin
+			os.Stdin = tmpfileIN
+			defer func() {
+				os.Stdin = oldstdin
+			}()
+			s := NewShell("./")
+			var w sync.WaitGroup
+			w.Add(1)
+			s.cmd(tmpfileIN, &w)
+			w.Wait()
+			if _, err := tmpfileOUT.Seek(0, 0); err != nil {
+				log.Fatal(err)
+			}
+			builder := strings.Builder{}
+			b := make([]byte, 1024)
+			for {
+				n, err := tmpfileOUT.Read(b)
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					t.Error(err.Error())
+					return
+				}
+				builder.Write(b[:n])
+			}
+			result := builder.String()
+			for _, wanted := range tt.want {
+				if !strings.Contains(result, wanted) {
+					t.Errorf("Wanted '%s' inside the string '%s'", wanted, result)
+				}
+			}
+		})
+	}
+}
